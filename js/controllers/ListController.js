4e19742fd4de6e501bbdfc4e217abdf5637a7b8c
chopperkuang <chopperkuang@gmail.com>
2013-02-26 15:06:31 +0800

    个性变更命名方式，采用驼峰式命名

--- js/controllers/ListController.js
+++ js/controllers/ListController.js
@@ -5,10 +5,10 @@
  */
 app.controller('ListController', function ListController($scope, $http, CustomerService) {
 
-    $scope.popup_show = 'hidden';
-    $scope.popup_select_show = 'hidden';
-    $scope.iframe_show = 'hidden';
-    $scope.sidebar_show = '';
+    $scope.popupShow = 'hidden';
+    $scope.popupSelectShow = 'hidden';
+    $scope.iframeShow = 'hidden';
+    $scope.sidebarShow = '';
 
     CustomerService.privateList(83639).then(function(data){
         $scope.customerList = data;
@@ -17,36 +17,36 @@ app.controller('ListController', function ListController($scope, $http, Customer
 
     // to show the popup window of calling
     $scope.call = function(num) {
-        this.popup_show = '';
-        this.iframe_show = '';
+        this.popupShow = '';
+        this.iframeShow = '';
     };
 
     // to hide the popup window of calling
     $scope.callCancel = function(){
-        this.popup_show = 'hidden';
-        this.iframe_show = 'hidden';
+        this.popupShow = 'hidden';
+        this.iframeShow = 'hidden';
     };
 
     // to show the popup window of selecting phone number
     $scope.callSelect = function(type){
         this.call_type = type;
-        this.popup_show = 'hidden';
-        this.popup_select_show = '';
+        this.popupShow = 'hidden';
+        this.popupSelectShow = '';
     };
 
     // to hide the popup window of selecting phone number
     $scope.selectCancel = function(){
-        this.popup_select_show = 'hidden';
-        this.iframe_show = 'hidden';
+        this.popupSelectShow = 'hidden';
+        this.iframeShow = 'hidden';
     }
 
     // to show or hide sidebar
     $scope.sidebarSwitch = function(){
-        if(this.sidebar_show == ''){
-            this.sidebar_show = 'sidebar_show';
+        if(this.sidebarShow == ''){
+            this.sidebarShow = 'sidebarShow';
         }
         else{
-            this.sidebar_show = '';
+            this.sidebarShow = '';
         }
     }
 
